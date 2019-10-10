a5111dfb098f04e173f9fbb3eadfaf68b51acbc7
BuskoViktoryia <Viktoryia.Busko@gmail.com>
2019-10-11 00:34:42 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(preferences, knowsProgramming, config) {
+  var speed = config[preferences];
+  var sum = knowsProgramming ? 800 : 1300;
+  var weeks = sum / speed;
+  return Math.ceil(weeks);
+}
